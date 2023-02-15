b0e01a2390624de8ebc0f0331853dccb33c99368
wuying <1543007289@qq.com>
2023-02-15 17:39:41 +0800

    1.新增轮播组件carousel 2.添加app页面

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import MyCarousel from './component/Carousel/index.tsx'
+import airpods from './assets/airpods.png'
+import iphone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+
+function App() {
+  const arr1 = [
+    { title: 'xPhone',desc:'Lots to love. Less to spend.',prices:'Starting at $399.',style:{ color:'white',backgroundColor:'#101010'}, url: `url(${iphone})` },
+    { title: 'Tablet',desc:'Just the right amount of everything.',prices:'',style:{color:'black'}, url: `url(${tablet})` }, 
+    { title: 'Buy a Tablet or xPhone for college.Get arPods.',desc:'',prices:'',style:{color:'black'}, url: `url(${airpods})` }
+  ]
+  return (
+    <div className='App'>
+      <MyCarousel arr={arr1} />
+    </div>
+  )
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-// import './index.css'
 
-// function App() {
-//   return (
-//     <div>
-//       <p>hello</p>
-//     </div>
-//   )
-// }
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <div>
-      <p>hello</p>
-    </div>
+    <App/>
   </React.StrictMode>
 );
 
 
-// export default App;
